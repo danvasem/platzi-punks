a4338994ccf44a80a20ac7252f1c987f317ee742
danvasem <turok3000@gmail.com>
2022-01-31 21:23:36 -0500

    Ajuste en la utilización de ambientes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RINKEBY_RPC_URL = process.env.INFURA_PROJECT_ID;
+const PRIVATE_KEY = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -11,7 +11,7 @@ module.exports = {
   solidity: "0.8.4",
   networks:{
     rinkeby:{
-      url: 'https://rinkeby.infura.io/v3/'+RINKEBY_RPC_URL,
+      url: `https://rinkeby.infura.io/v3/${RINKEBY_RPC_URL}`,
       accounts:[
         PRIVATE_KEY
       ]
